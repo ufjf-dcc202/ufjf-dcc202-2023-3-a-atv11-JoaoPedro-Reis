5f00c47db1b20e15a134f622f179a2f5ff23090f
Joao Pedro Reis <joaoreis.pedro@estudante.ufjf.br>
2023-11-29 20:37:05 -0300

    mais ajustes no index, main e lista

--- lista.js
+++ lista.js
@@ -3,3 +3,7 @@ const lista = ["Um", "Dois", "Tres"];
 export function getLista(){
     return lista;
 }
+
+export function limparLista(){
+    lista.splice(0, 3);
+}
--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-import {getLista} from "./lista.js";
+import {getLista, limparLista} from "./lista.js";
 
 const pEntrada = document.querySelector('#entrada');
 const olSaida = document.querySelector('#saida');
 const btnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
 
-olSaida.innerHTML = "";
+btnLimpar.addEventListener('click', limparElementosDaLista);
 
-function atualizarItensDeLista(params){
-    
+atualizarItensDeLista();
+
+function limparElementosDaLista(){
+    limparLista();
+    atualizarItensDeLista();
+}
+
+function atualizarItensDeLista(){
+    olSaida.innerHTML = "";
+    const lista = getLista();
+    for(let i = 0; i < lista.length;i++){
+        const item = lista[i];
+        const li = document.createElement('li');
+        li.textContent = item;
+        olSaida.appendChild(li);
+    }
+
 }
